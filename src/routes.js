e70dd85d0aa2bf6653964c09ee03c89566e78d4d
KIMWOOJUNG <gnujoow.kim@gmail.com>
2016-08-14 03:04:28 +0900

    redux basic build up
    npm package installed redux-promise & redux
    action created

--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,8 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostIndex from './components/post_index';
 
-const Greeting = () => {
-  return <div>ㅇ안뇽</div>
-};
-
 export default(
   <Route path='/' component={App}>
     <IndexRoute component={PostIndex} />
-    <Route path="greet" component={Greeting} />
-    <Route path="greet2" component={Greeting} />
-    <Route path="greet3" component={Greeting} />
   </Route>
 );
