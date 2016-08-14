5c423a7aaf56eecd957bd7bcb81db2e1ca974bcb
KIMWOOJUNG <gnujoow.kim@gmail.com>
2016-08-14 15:33:53 +0900

    create from added / component & route

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { fetchPosts } from '../actions/index';
 
 class PostIndex extends React.Component {
---
+++ src/components/post_new.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PostNew extends React.Component {
+  render (){
+    return (
+      <div>입력 폼</div>
+    );
+  }
+}
+
+export default PostNew;
