dc6fd11418543a955853785e1c168fea23ee8cb1
KIMWOOJUNG <gnujoow.kim@gmail.com>
2016-08-14 18:52:45 +0900

    pass entire class for each input fields

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -11,17 +11,17 @@ class PostNew extends Component {
         <h3>포스트 만들기 </h3>
         <div className="form-group">
           <label>제목</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...title} />
         </div>
 
         <div className="form-group">
           <label>분류</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {..categories}/>
         </div>
 
         <div className="form-group">
           <label>내용</label>
-          <textarea className="form-control" />
+          <textarea className="form-control" {...content} />
         </div>
 
         <button type="submit" className="btn btn-primary">보내기</button>
