1cc0c18d6c9ec18562728e56e4ec8659163b5232
KIMWOOJUNG <gnujoow.kim@gmail.com>
2016-08-14 19:15:38 +0900

    post sucess yay \0/

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 
+import { createPost } from '../actions/index';
+
 class PostNew extends Component {
   render () {
     const { fields: {title, categories, content}, handleSubmit } = this.props;
-    console.log(title);
 
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>포스트 만들기 </h3>
         <div className="form-group">
           <label>제목</label>
@@ -16,7 +17,7 @@ class PostNew extends Component {
 
         <div className="form-group">
           <label>분류</label>
-          <input type="text" className="form-control" {..categories}/>
+          <input type="text" className="form-control" {...categories}/>
         </div>
 
         <div className="form-group">
@@ -30,8 +31,11 @@ class PostNew extends Component {
   }
 }
 
-// user types something in.. record it on application state
+
+// connect: first argument is mapToStateToProps, 2nd is mapDispatchToProps
+// reduxForm : 1st is form config 2nd is mapToStateToProps 3rd is mapDispatchToProps
+
 export default reduxForm({
   form: 'PostNewForm',
   fields: ['title', 'categories', 'content' ]
-})(PostNew);
+},null, { createPost })(PostNew);
