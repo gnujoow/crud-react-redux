f41dfeb529c68441e2d7a796b7c457db3c457ac8
KIMWOOJUNG <gnujoow.kim@gmail.com>
2016-08-16 03:28:13 +0900

    fetch & render postShow

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -7,12 +7,25 @@ class PostShow extends React.Component {
   componentWillMount() {
     this.props.fetchPost(this.props.params.id);
   }
-  
+
   render(){
+    const { post } = this.props;
+    if(!post){
+      return <div>로오딩중우우웅...</div>
+    }
     return (
-      <div>{this.props.params.id}</div>
+      <div>
+        <h3>{this.props.params.id} :: {post.title}</h3>
+        <h6>{post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
+
     );
   }
 }
 
-export default connect(null, { fetchPost })(PostShow);
+function mapStateToProps(state){
+  return { post: state.posts.post };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostShow);
